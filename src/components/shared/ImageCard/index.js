19ac12372dd1515aab8f527924439e081e3f875d
kevin Martinez <kevin.martinez@dnamicworld.com>
2019-07-29 12:49:40 -0600

    Add public api for random images. Create Loading component

--- src/components/shared/ImageCard/index.js
+++ src/components/shared/ImageCard/index.js
@@ -2,86 +2,90 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import StackGrid from "react-stack-grid";
 
+const randomIdImage = () => {
+  return Math.round(Math.random() * (1016 - 1001) + 1001);
+};
 class CardImage extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super()
     this.state = {
       perPage: 10,
-      images : [
+      images: [
         {
-          width : '200',
-          height : '400',
+          width: '200',
+          height: '400',
         },
         {
-          width : '300',
-          height : '400',
+          width: '300',
+          height: '400',
         },
         {
-          width : '400',
-          height : '200',
+          width: '400',
+          height: '200',
         },
         {
-          width : '400',
-          height : '200',
+          width: '400',
+          height: '200',
         },
         {
-          width : '200',
-          height : '400',
+          width: '200',
+          height: '400',
         },
         {
-          width : '300',
-          height : '400',
+          width: '300',
+          height: '400',
         },
         {
-          width : '400',
-          height : '200',
+          width: '400',
+          height: '200',
         },
         {
-          width : '400',
-          height : '200',
+          width: '400',
+          height: '200',
         },
         {
-          width : '400',
-          height : '200',
+          width: '400',
+          height: '200',
         },
         {
-          width : '200',
-          height : '400',
+          width: '200',
+          height: '400',
         },
         {
-          width : '300',
-          height : '400',
+          width: '300',
+          height: '400',
         },
         {
-          width : '400',
-          height : '200',
+          width: '400',
+          height: '200',
         },
         {
-          width : '400',
-          height : '200',
+          width: '400',
+          height: '200',
         },
       ]
     }
   }
-  render () {
+  render() {
+    console.log('Random number', randomIdImage())
     return (
       <StackGrid
         columnWidth={300}
       >
-      {
-        this.state.images.map((e, index) => (
-          <div key={index}>
-            <div className="card" style={{ width: '18rem' }}>
-              <img src={`https://dummyimage.com/${e.width}x${e.height}/000/fff`} className="card-img-top" alt="..."/>
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <Link to="/home" className="btn btn-primary">Go somewhere</Link>
+        {
+          this.state.images.map((e, index) => (
+            < div key={index} >
+              <div className="card" style={{ width: '18rem' }}>
+                <img src={`https://picsum.photos/id/${randomIdImage()}/${e.width}/${e.height}`} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <Link to="/home" className="btn btn-primary">Go somewhere</Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))
-      }
+          ))
+        }
       </StackGrid>
     );
   }
---
+++ src/components/shared/Loading/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './styles.css';
+
+export default function () {
+  return (
+    <div class="lds-default">
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+      <div>
+      </div>
+    </div>
+  )
+}
